6a61d0dd3ed97526abd19da31affd310c5266dda
Rudolf Polzer <divverent@xonotic.org>
2014-08-26 14:47:40 +0200

    Prepare to be able to do a hack for linux32.

--- .travis-before_install-xonotic.sh
+++ .travis-before_install-xonotic.sh
@@ -6,26 +6,26 @@ if [ "`uname`" = 'Linux' ]; then
   sudo apt-get update -qq
 fi
 
-case "$1" in
-  linux32)
-    sudo apt-get install -y libc6-dev-i386
-    ;;
-  linux64)
-    sudo apt-get install -y libxpm-dev libsdl1.2-dev libxxf86vm-dev
-    ;;
-  win32)
-    sudo apt-get install -y mingw-w64 mingw32- mingw32-binutils-
-    ;;
-  win64)
-    sudo apt-get install -y mingw-w64 mingw32- mingw32-binutils-
-    ;;
-  osx)
-    ;;
-esac
+for os in "$@"; do
+  case "$os" in
+    linux32)
+      sudo apt-get install -y libc6-dev-i386
+      ;;
+    linux64)
+      sudo apt-get install -y libxpm-dev libsdl1.2-dev libxxf86vm-dev
+      ;;
+    win32)
+      sudo apt-get install -y mingw-w64 mingw32- mingw32-binutils-
+      ;;
+    win64)
+      sudo apt-get install -y mingw-w64 mingw32- mingw32-binutils-
+      ;;
+    osx)
+      ;;
+  esac
+done
 
-if ! [ -d .deps ]; then
-  git archive --format=tar --remote=git://de.git.xonotic.org/xonotic/xonotic.git --prefix=.deps/ master:misc/builddeps | tar xvf -
-  for X in .deps/*; do
-    rsync --remove-source-files -vaSHP -L "$X"/*/ "$X"/ || true
-  done
-fi
+git archive --format=tar --remote=git://de.git.xonotic.org/xonotic/xonotic.git --prefix=.deps/ master:misc/builddeps | tar xvf -
+for X in .deps/*; do
+  rsync --remove-source-files -vaSHP -L "$X"/*/ "$X"/ || true
+done
--- .travis-script-xonotic.sh
+++ .travis-script-xonotic.sh
@@ -1,40 +1,43 @@
 #!/bin/sh
 
-os=$1
-
 set -ex
 
-deps=".deps/${os}"
+for os in "$@"; do
 
-case "${os}" in
-	linux32)
-		makeflags='STRIP=: CC="${CC} -m32 -march=i686 -g1 -I../../../${deps}/include -L../../../${deps}/lib -DSUPPORTIPV6" LIB_JPEG=../../../${deps}/lib/libjpeg.a LIB_CRYPTO="../../../${deps}/lib/libd0_blind_id.a ../../../${deps}/lib/libgmp.a" DP_LINK_ZLIB=shared DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=shared DP_LINK_CRYPTO_RIJNDAEL=dlopen'
-		maketargets='release'
-		outputs='darkplaces-glx darkplaces-sdl darkplaces-dedicated'
-		;;
-	linux64)
-		makeflags='STRIP=: CC="${CC} -m64 -g1 -I../../../${deps}/include -L../../../${deps}/lib -DSUPPORTIPV6" LIB_JPEG=../../../${deps}/lib/libjpeg.a LIB_CRYPTO="../../../${deps}/lib/libd0_blind_id.a ../../../${deps}/lib/libgmp.a" DP_LINK_ZLIB=shared DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=shared DP_LINK_CRYPTO_RIJNDAEL=dlopen'
-		maketargets='release'
-		outputs='darkplaces-glx darkplaces-sdl darkplaces-dedicated'
-		;;
-	win32)
-		makeflags='STRIP=: DP_MAKE_TARGET=mingw UNAME=MINGW32 CC="i686-w64-mingw32-gcc -g1 -Wl,--dynamicbase -Wl,--nxcompat -I../../../${deps}/include -L../../../${deps}/lib -DUSE_WSPIAPI_H -DSUPPORTIPV6" WINDRES="i686-w64-mingw32-windres" SDL_CONFIG="../../../${deps}/bin/sdl-config" DP_LINK_ZLIB=dlopen DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=dlopen DP_LINK_CRYPTO_RIJNDAEL=dlopen WIN32RELEASE=1 D3D=1'
-		maketargets='release'
-		outputs='darkplaces.exe darkplaces-sdl.exe darkplaces-dedicated.exe'
-		;;
-	win64)
-		makeflags='STRIP=: DP_MAKE_TARGET=mingw UNAME=MINGW32 CC="x86_64-w64-mingw32-gcc -g1 -Wl,--dynamicbase -Wl,--nxcompat -I../../../${deps}/include -L../../../${deps}/lib -DSUPPORTIPV6" WINDRES="x86_64-w64-mingw32-windres" SDL_CONFIG="../../../${deps}/bin/sdl-config" DP_LINK_ZLIB=dlopen DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=dlopen DP_LINK_CRYPTO_RIJNDAEL=dlopen WIN64RELEASE=1 D3D=1'
-		maketargets='release'
-		outputs='darkplaces.exe darkplaces-sdl.exe darkplaces-dedicated.exe'
-		;;
-	osx)
-		makeflags='STRIP=: CC="gcc -g1 -arch i386 -arch ppc -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.4 -I../../../${deps}/include -L../../../${deps}/lib -fno-reorder-blocks -DSUPPORTIPV6" DP_LINK_ZLIB=shared DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=dlopen DP_LINK_CRYPTO_RIJNDAEL=dlopen'
-		maketargets='sv-release sdl-release'
-		outputs='darkplaces-sdl darkplaces-dedicated'
-		;;
-esac
+  deps=".deps/${os}"
+  case "${os}" in
+    linux32)
+      makeflags='STRIP=: CC="${CC} -m32 -march=i686 -g1 -I../../../${deps}/include -L../../../${deps}/lib -DSUPPORTIPV6" LIB_JPEG=../../../${deps}/lib/libjpeg.a LIB_CRYPTO="../../../${deps}/lib/libd0_blind_id.a ../../../${deps}/lib/libgmp.a" DP_LINK_ZLIB=shared DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=shared DP_LINK_CRYPTO_RIJNDAEL=dlopen'
+      maketargets='release'
+      outputs='darkplaces-glx darkplaces-sdl darkplaces-dedicated'
+      ;;
+    linux64)
+      makeflags='STRIP=: CC="${CC} -m64 -g1 -I../../../${deps}/include -L../../../${deps}/lib -DSUPPORTIPV6" LIB_JPEG=../../../${deps}/lib/libjpeg.a LIB_CRYPTO="../../../${deps}/lib/libd0_blind_id.a ../../../${deps}/lib/libgmp.a" DP_LINK_ZLIB=shared DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=shared DP_LINK_CRYPTO_RIJNDAEL=dlopen'
+      maketargets='release'
+      outputs='darkplaces-glx darkplaces-sdl darkplaces-dedicated'
+      ;;
+    win32)
+      makeflags='STRIP=: DP_MAKE_TARGET=mingw UNAME=MINGW32 CC="i686-w64-mingw32-gcc -g1 -Wl,--dynamicbase -Wl,--nxcompat -I../../../${deps}/include -L../../../${deps}/lib -DUSE_WSPIAPI_H -DSUPPORTIPV6" WINDRES="i686-w64-mingw32-windres" SDL_CONFIG="../../../${deps}/bin/sdl-config" DP_LINK_ZLIB=dlopen DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=dlopen DP_LINK_CRYPTO_RIJNDAEL=dlopen WIN32RELEASE=1 D3D=1'
+      maketargets='release'
+      outputs='darkplaces.exe darkplaces-sdl.exe darkplaces-dedicated.exe'
+      ;;
+    win64)
+      makeflags='STRIP=: DP_MAKE_TARGET=mingw UNAME=MINGW32 CC="x86_64-w64-mingw32-gcc -g1 -Wl,--dynamicbase -Wl,--nxcompat -I../../../${deps}/include -L../../../${deps}/lib -DSUPPORTIPV6" WINDRES="x86_64-w64-mingw32-windres" SDL_CONFIG="../../../${deps}/bin/sdl-config" DP_LINK_ZLIB=dlopen DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=dlopen DP_LINK_CRYPTO_RIJNDAEL=dlopen WIN64RELEASE=1 D3D=1'
+      maketargets='release'
+      outputs='darkplaces.exe darkplaces-sdl.exe darkplaces-dedicated.exe'
+      ;;
+    osx)
+      makeflags='STRIP=: CC="gcc -g1 -arch i386 -arch ppc -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.4 -I../../../${deps}/include -L../../../${deps}/lib -fno-reorder-blocks -DSUPPORTIPV6" DP_LINK_ZLIB=shared DP_LINK_JPEG=dlopen DP_LINK_ODE=dlopen DP_LINK_CRYPTO=dlopen DP_LINK_CRYPTO_RIJNDAEL=dlopen'
+      maketargets='sv-release sdl-release'
+      outputs='darkplaces-sdl darkplaces-dedicated'
+      ;;
+  esac
 
-trap 'make clean' EXIT
-eval "make ${makeflags} ${maketargets}"
-mkdir "output.${os}"
-mv ${outputs} "output.${os}"/
+  (
+  trap "make ${makeflags} clean" EXIT
+  eval "make ${makeflags} ${maketargets}"
+  mkdir "output.${os}"
+  mv ${outputs} "output.${os}"/
+  )
+
+done
