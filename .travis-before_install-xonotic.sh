aabc7f1bc4ca8afe84915d5f1405caeef7f3d474
Rudolf Polzer <divverent@xonotic.org>
2014-08-26 14:30:24 +0200

    More travis stuff :)

--- .travis-before_install-xonotic.sh
+++ .travis-before_install-xonotic.sh
@@ -3,16 +3,29 @@
 set -ex
 
 if [ "`uname`" = 'Linux' ]; then
-	sudo apt-get update -qq
-	# linux64 parts.
-	sudo apt-get install -y libxpm-dev
-	# linux32 parts.
-	sudo apt-get install -y libc6-dev-i386
-	# win64/win32 parts.
-	sudo apt-get install -y mingw-w64 mingw32- mingw32-binutils-
+  sudo apt-get update -qq
 fi
 
-git archive --format=tar --remote=git://de.git.xonotic.org/xonotic/xonotic.git --prefix=.deps/ master:misc/builddeps | tar xvf -
-for X in .deps/*; do
-	rsync --remove-source-files -vaSHP -L "$X"/*/ "$X"/ || true
-done
+case "$1" in
+  linux32)
+    sudo apt-get install -y libc6-dev-i386
+    ;;
+  linux64)
+    sudo apt-get install -y libxpm-dev libsdl2-dev
+    ;;
+  win32)
+    sudo apt-get install -y mingw-w64 mingw32- mingw32-binutils-
+    ;;
+  win64)
+    sudo apt-get install -y mingw-w64 mingw32- mingw32-binutils-
+    ;;
+  osx)
+    ;;
+esac
+
+if ! [ -d .deps ]; then
+  git archive --format=tar --remote=git://de.git.xonotic.org/xonotic/xonotic.git --prefix=.deps/ master:misc/builddeps | tar xvf -
+  for X in .deps/*; do
+    rsync --remove-source-files -vaSHP -L "$X"/*/ "$X"/ || true
+  done
+fi
